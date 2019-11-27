9518b52998d39e5e37811eab9eeadc333e3704f2
linxcodev <fajrul452@gmail.com>
2019-11-27 09:20:05 +0700

    init project & socket message

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+// permission file assets
+app.use(express.static('assets'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+});
+
+io.on('connection', socket => {
+  // mengirim pesan
+  socket.on('newMessage', (msg) => {
+    io.emit('newMessage', msg)
+  })
+});
+
+http.listen(2000, () => {
+  console.log('listening on *:2000')
+});
